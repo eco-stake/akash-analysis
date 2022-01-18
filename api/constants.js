99221165fc5b1887b3839ae49897d063fee088f2
Maxime Cyr <max_maxime_max@hotmail.com>
2022-01-18 12:20:33 -0500

    Remove old api (#34)
    * Remove old api
    
    * Remove cahe folder from .gitignore

--- api/constants.js
+++
@@ -1,5 +0,0 @@
-exports.averageBlockTime = 6.174;
-
-exports.mainNet = "https://raw.githubusercontent.com/ovrclk/net/master/mainnet";
-exports.testNet = "https://raw.githubusercontent.com/ovrclk/net/master/testnet";
-exports.edgeNet = "https://raw.githubusercontent.com/ovrclk/net/master/edgenet";
