febe677d717cfbccd6d8d52bd85bf0f22fc75482
Redm4x <max_maxime_max@hotmail.com>
2022-07-10 03:00:50 -0400

    Reuse code for genesis vs msg

--- api/src/akash/handlers/proposal.ts
+++ api/src/akash/handlers/proposal.ts
@@ -8,7 +8,7 @@ import { CommunityPoolSpendProposal } from "cosmjs-types/cosmos/distribution/v1b
 import { TextProposal } from "cosmjs-types/cosmos/gov/v1beta1/gov";
 
 interface GenesisProposal {
-  content: ParameterChangeProposal | SoftwareUpgradeProposal;
+  content: ParameterChangeProposal | SoftwareUpgradeProposal | CommunityPoolSpendProposal | TextProposal;
   deposit_end_time: string;
   final_tally_result: {
     abstain: string;
@@ -48,52 +48,22 @@ export async function createProposalFromMsg(msgSubmitProposal: MsgSubmitProposal
   switch (msgSubmitProposal.content.typeUrl) {
     case "/cosmos.params.v1beta1.ParameterChangeProposal":
       const parameterChangeProposal = ParameterChangeProposal.decode(msgSubmitProposal.content.value);
-      proposal.title = parameterChangeProposal.title;
-      proposal.description = parameterChangeProposal.description;
-
-      proposalParameterChanges = parameterChangeProposal.changes.map((change) =>
-        ProposalParameterChange.build({
-          id: uuid.v4(),
-          proposalId: proposalId,
-          subspace: change.subspace,
-          key: change.key,
-          value: change.value
-        })
-      );
+      await processParameterChangeProposal(proposal, parameterChangeProposal, blockGroupTransaction);
       break;
 
     case "/cosmos.upgrade.v1beta1.SoftwareUpgradeProposal":
       const softwareUpgradeProposal = SoftwareUpgradeProposal.decode(msgSubmitProposal.content.value);
-
-      if (!softwareUpgradeProposal.plan) {
-        throw new Error("Software upgrade proposal must have a plan");
-      }
-
-      proposal.title = softwareUpgradeProposal.title;
-      proposal.description = softwareUpgradeProposal.description;
-      proposal.softwareUpgradeName = softwareUpgradeProposal.plan.name;
-      proposal.softwareUpgradeHeight = softwareUpgradeProposal.plan.height.toNumber();
-      proposal.softwareUpgradeInfo = softwareUpgradeProposal.plan.info;
+      processSoftwareUpgradeProposal(proposal, softwareUpgradeProposal);
       break;
 
     case "/cosmos.gov.v1beta1.TextProposal":
       const textProposal = TextProposal.decode(msgSubmitProposal.content.value);
-
-      proposal.title = textProposal.title;
-      proposal.description = textProposal.description;
+      processTextProposal(proposal, textProposal);
       break;
 
     case "/cosmos.distribution.v1beta1.CommunityPoolSpendProposal":
       const communityPoolSpendProposal = CommunityPoolSpendProposal.decode(msgSubmitProposal.content.value);
-
-      if (communityPoolSpendProposal.amount.length > 1) {
-        throw new Error("Community pool spend proposal must have a single amount");
-      }
-
-      proposal.title = communityPoolSpendProposal.title;
-      proposal.description = communityPoolSpendProposal.description;
-      proposal.recipient = communityPoolSpendProposal.recipient;
-      proposal.amount = coinToUAkt(communityPoolSpendProposal.amount[0]);
+      processCommunityPoolSpendProposal(proposal, communityPoolSpendProposal);
 
     default:
       throw Error("Unsupported proposal type: " + msgSubmitProposal.content.typeUrl);
@@ -105,11 +75,7 @@ export async function createProposalFromMsg(msgSubmitProposal: MsgSubmitProposal
   }
 }
 
-export async function createProposalFromGenesis(genesisProposal: GenesisProposal, blockGroupTransaction) {
-  // if (genesisProposal.initialDeposit.length > 1) {
-  //   throw new Error("Initial deposit is not supported");
-  // }
-
+export async function createProposalFromGenesis(genesisProposal: GenesisProposal, dbTransaction) {
   let proposal = Proposal.build({
     id: parseInt(genesisProposal.proposal_id),
     type: genesisProposal.content["@type"]
@@ -119,60 +85,73 @@ export async function createProposalFromGenesis(genesisProposal: GenesisProposal
 
   switch (genesisProposal.content["@type"]) {
     case "/cosmos.params.v1beta1.ParameterChangeProposal":
-      const parameterChangeProposal = genesisProposal.content as ParameterChangeProposal;
-      proposal.title = parameterChangeProposal.title;
-      proposal.description = parameterChangeProposal.description;
-
-      proposalParameterChanges = parameterChangeProposal.changes.map((change) =>
-        ProposalParameterChange.build({
-          id: uuid.v4(),
-          proposalId: genesisProposal.proposal_id,
-          subspace: change.subspace,
-          key: change.key,
-          value: change.value
-        })
-      );
+      await processParameterChangeProposal(proposal, genesisProposal.content as ParameterChangeProposal, dbTransaction);
       break;
 
     case "/cosmos.upgrade.v1beta1.SoftwareUpgradeProposal":
-      const softwareUpgradeProposal = genesisProposal.content as SoftwareUpgradeProposal;
-
-      if (!softwareUpgradeProposal.plan) {
-        throw new Error("Software upgrade proposal must have a plan");
-      }
-
-      proposal.title = softwareUpgradeProposal.title;
-      proposal.description = softwareUpgradeProposal.description;
-      proposal.softwareUpgradeName = softwareUpgradeProposal.plan.name;
-      proposal.softwareUpgradeHeight = softwareUpgradeProposal.plan.height.toNumber();
-      proposal.softwareUpgradeInfo = softwareUpgradeProposal.plan.info;
+      processSoftwareUpgradeProposal(proposal, genesisProposal.content as SoftwareUpgradeProposal);
       break;
 
     case "/cosmos.gov.v1beta1.TextProposal":
-      const textProposal = genesisProposal.content as TextProposal;
-
-      proposal.title = textProposal.title;
-      proposal.description = textProposal.description;
+      processTextProposal(proposal, genesisProposal.content as TextProposal);
       break;
 
     case "/cosmos.distribution.v1beta1.CommunityPoolSpendProposal":
-      const communityPoolSpendProposal = genesisProposal.content as CommunityPoolSpendProposal;
-
-      if (communityPoolSpendProposal.amount.length > 1) {
-        throw new Error("Community pool spend proposal must have a single amount");
-      }
-
-      proposal.title = communityPoolSpendProposal.title;
-      proposal.description = communityPoolSpendProposal.description;
-      proposal.recipient = communityPoolSpendProposal.recipient;
-      proposal.amount = coinToUAkt(communityPoolSpendProposal.amount[0]);
+      processCommunityPoolSpendProposal(proposal, genesisProposal.content as CommunityPoolSpendProposal);
+      break;
 
     default:
       throw Error("Unsupported proposal type: " + genesisProposal.content["@type"]);
   }
 
-  await proposal.save({ transaction: blockGroupTransaction });
+  await proposal.save({ transaction: dbTransaction });
   for (const proposalParameterChange of proposalParameterChanges) {
-    await proposalParameterChange.save({ transaction: blockGroupTransaction });
+    await proposalParameterChange.save({ transaction: dbTransaction });
+  }
+}
+
+function processTextProposal(proposal: Proposal, textProposal: TextProposal) {
+  proposal.title = textProposal.title;
+  proposal.description = textProposal.description;
+}
+
+function processCommunityPoolSpendProposal(proposal: Proposal, communityPoolSpendProposal: CommunityPoolSpendProposal) {
+  if (communityPoolSpendProposal.amount.length > 1) {
+    throw new Error("Community pool spend proposal must have a single amount");
+  }
+
+  proposal.title = communityPoolSpendProposal.title;
+  proposal.description = communityPoolSpendProposal.description;
+  proposal.recipient = communityPoolSpendProposal.recipient;
+  proposal.amount = coinToUAkt(communityPoolSpendProposal.amount[0]);
+}
+
+function processSoftwareUpgradeProposal(proposal: Proposal, softwareUpgradeProposal: SoftwareUpgradeProposal) {
+  if (!softwareUpgradeProposal.plan) {
+    throw new Error("Software upgrade proposal must have a plan");
+  }
+
+  proposal.title = softwareUpgradeProposal.title;
+  proposal.description = softwareUpgradeProposal.description;
+  proposal.softwareUpgradeName = softwareUpgradeProposal.plan.name;
+  proposal.softwareUpgradeHeight = softwareUpgradeProposal.plan.height.toNumber();
+  proposal.softwareUpgradeInfo = softwareUpgradeProposal.plan.info;
+}
+
+async function processParameterChangeProposal(proposal: Proposal, parameterChangeProposal: ParameterChangeProposal, dbTransaction) {
+  proposal.title = parameterChangeProposal.title;
+  proposal.description = parameterChangeProposal.description;
+
+  for (const change of parameterChangeProposal.changes) {
+    await ProposalParameterChange.create(
+      {
+        id: uuid.v4(),
+        proposalId: proposal.id,
+        subspace: change.subspace,
+        key: change.key,
+        value: change.value
+      },
+      { transaction: dbTransaction }
+    );
   }
 }
